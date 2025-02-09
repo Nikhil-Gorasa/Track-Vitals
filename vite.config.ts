a02e02a5ee153e1aa152d2a5c98fb5b3516185bc
Nikhil Gorasa <nikhil.gorasa1@gmail.com>
2025-02-09 20:28:29 +0530

    Minor Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,7 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    allowedHosts: ['93d2-2409-40f0-29-f353-8dc8-ef3e-8bd3-df00.ngrok-free.app']
+  }
 });
